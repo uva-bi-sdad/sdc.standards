aadcba55736f074fcf59589224f3c59001b6170d
Aaron Schroeder <dads2busy@gmail.com>
2022-09-21 16:09:25 +0000

    adding repos

--- src/data_categories_topics.R
+++ src/data_categories_topics.R
@@ -3,4 +3,6 @@ library(data.table)
 
 data_cats_tops <- fread("src/data_categories_topics.csv")
 reactable(data_cats_tops,
-          groupBy = c("REPO", "DATATYPE", "DATACATEGORY"))
+          groupBy = c("RepoName", "DataType", "DataCategory"),
+          compact = TRUE,
+          style = "font-size: 14px;")
